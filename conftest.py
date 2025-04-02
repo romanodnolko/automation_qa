0d6783f66aa95833064a9e426b6512228aabc860
Roman <rodnolko@united-internet.de>
2025-04-02 15:15:12 +0200

    create a project, packgages and files

---
+++ conftest.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
